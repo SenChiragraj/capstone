94540b3745ce7bae3265d3b7404b90bc8523a92c
kuchulakz <95769349+kuchulakz@users.noreply.github.com>
2025-02-08 12:41:45 +0000

    User & Patient

--- server/src/main/java/com/wecp/healthcare_appointment_management_system/entity/Patient.java
+++ server/src/main/java/com/wecp/healthcare_appointment_management_system/entity/Patient.java
@@ -3,7 +3,33 @@ package com.wecp.healthcare_appointment_management_system.entity;
 import javax.persistence.*;
 import java.util.Set;
 
+@Entity
+public class Patient extends User 
+{
+    Set<MedicalRecord> medicalRecords;
+    Set<Appointment> appointments;
 
-public class Patient extends User {
+    public Patient()
+    {
+        super();
+    }
+
+    public Set<MedicalRecord> getMedicalRecords() {
+        return medicalRecords;
+    }
+
+    public void setMedicalRecords(Set<MedicalRecord> medicalRecords) {
+        this.medicalRecords = medicalRecords;
+    }
+
+    public Set<Appointment> getAppointments() {
+        return appointments;
+    }
+
+    public void setAppointments(Set<Appointment> appointments) {
+        this.appointments = appointments;
+    }
+
+    
     // implement patient entity
 }
--- server/src/main/java/com/wecp/healthcare_appointment_management_system/service/UserService.java
+++ server/src/main/java/com/wecp/healthcare_appointment_management_system/service/UserService.java
@@ -1,5 +1,7 @@
 package com.wecp.healthcare_appointment_management_system.service;
 
+import com.wecp.healthcare_appointment_management_system.dto.LoginRequest;
+import com.wecp.healthcare_appointment_management_system.dto.LoginResponse;
 import com.wecp.healthcare_appointment_management_system.entity.Doctor;
 import com.wecp.healthcare_appointment_management_system.entity.Patient;
 import com.wecp.healthcare_appointment_management_system.entity.Receptionist;
@@ -9,15 +11,57 @@ import com.wecp.healthcare_appointment_management_system.repository.PatientRepos
 import com.wecp.healthcare_appointment_management_system.repository.ReceptionistRepository;
 import com.wecp.healthcare_appointment_management_system.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 
+@Service
+public class UserService  
+{
+    @Autowired
+    private UserRepository userRepository;
+    
+    
+    public Patient registerPatient(Patient patient) 
+    {
+        // register patient
+        return userRepository.save(patient);
+    }
 
-public class UserService  {
+    public Doctor registerDoctor(Doctor doctor) 
+    {
+        return userRepository.save(doctor);
+        // register doctor
+    }
 
+    public Receptionist registerReceptionist(Receptionist receptionist) 
+    {
+        return userRepository.save(receptionist);
+       // register receptionist
+    }
+    
+    //change the token
+    public LoginResponse loginUser(LoginRequest loginRequest) 
+    {
+        String token="token"; 
+        if(token == "token")
+        {
+            return new LoginResponse(null, token, token, token, token);
+        }
+        else
+        {
+            System.out.println("EXCEPTION 401");
+        }
+        return null;
+
+      // login user and return jwt in LoginResponse object
+        // return 401 unauthorized if authentication fail
+    }
 }
